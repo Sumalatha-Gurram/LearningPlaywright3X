aa54fe1205ef469425b23d53f8e61deb52cb7a24
Sumalatha-Gurram <sumalatha.gurram27@gmail.com>
2026-07-22 19:09:18 +0530

    Add operator examples for increment and decrement

---
+++ 04_chapter_Operators/16_Increment_Operators.js
@@ -0,0 +1,11 @@
+let a = 100;
+
+console.log(a++ + ++a +a++ + ++a);
+
+console.log(a);
+
+
+let i = 1; 
+
+let r = i++ > 1 ? i++ : ++i; 
+console.log(r, i);
---
+++ 04_chapter_Operators/17_Decrement_Operator.js
@@ -0,0 +1,9 @@
+let a = 37;
+
+console.log(--a + a--);
+
+console.log(a);
+
+let x = 5; 
+let y = x-- - --x; 
+console.log(y, x);
