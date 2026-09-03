7d023b6969060c41e007245f2f9c99d4a7c994ee
Sumalatha-Gurram <sumalatha.gurram27@gmail.com>
2026-09-03 21:10:52 +0530

    Add TypeScript and OOP examples

---
+++ 19_chapter_Oops_Polymorphism/200_Method_Overriding.js
@@ -0,0 +1,16 @@
+class BaseTest {
+    setup() {
+        console.log("Base: open browser");
+    }
+}
+
+class APIPage extends BaseTest{
+    setup() {
+        console.log("APITest: open browser");
+    }
+}
+
+let btest = new BaseTest();
+let test = new APIPage();
+test.setup();
+btest.setup();
